1a7b070378207fc284b8d8f041217595928e8148
Jan Zeugin <jan.zeugin@outlook.com>
2023-09-17 12:15:18 +0200

    Test Anpassen

--- tests/log/test_log.py
+++ tests/log/test_log.py
@@ -1,24 +1,44 @@
 from tests.config.config import (
     client,
-    override_get_db,
 )
-from he21_atl_material_lager.services.users import get_user_by_username
-from he21_atl_material_lager.services.logs import get_logs_by_user_id
 
 pytest_plugins = ["tests.config.fixture"]
 
 
-def test_log_get(valid_token_admin):
-    pass
+def test_log_get(valid_token_admin, valid_token_user):
+    response = client.get(
+        "/logs/", headers={"Authorization": f"Bearer {valid_token_admin}"}
+    )
+    assert response.status_code == 200, response.text
+    data = response.json()
 
-def get_log_by_user_id(valid_token_admin):
-    pass
+    assert "id" in data[0]
+    assert "datum" in data[0]
+    assert "log" in data[0]
+    assert "type" in data[0]
+    assert "created_by" in data[0]
+    assert "user_id" in data[0]
+    assert "item_id" in data[0]
 
-def get_log_by_log_id(valid_token_admin):
-    pass
 
-def get_log_by_log_type(valid_token_admin):
-    pass
+def test_get_log_by_user_id(valid_token_admin, create_user_admin):
+    response = client.get(
+        f"/logs/{create_user_admin}",
+        headers={"Authorization": f"Bearer {valid_token_admin}"},
+    )
+    assert response.status_code == 200, response.text
+    data = response.json()
 
-def get_log_by_created_by(valid_token_admin):
-    pass
+    assert data[0]["user_id"] == create_user_admin
+
+
+def test_get_log_by_log_id(valid_token_admin):
+    assert False
+
+
+def test_get_log_by_log_type(valid_token_admin):
+    assert False
+
+
+def test_get_log_by_created_by(valid_token_admin):
+    assert False
--- tests/log/test_log_item.py
+++ tests/log/test_log_item.py
@@ -1,17 +1,17 @@
 from tests.config.config import (
     client,
-    override_get_db,
 )
-from he21_atl_material_lager.services.users import get_user_by_username
-from he21_atl_material_lager.services.logs import get_logs_by_user_id
 
 pytest_plugins = ["tests.config.fixture"]
 
+
 def test_log_item_create(valid_token_admin):
-    pass
+    assert False
+
 
 def test_log_item_update(valid_token_admin):
-    pass
+    assert False
+
 
 def test_log_item_delete(valid_token_admin):
-    pass
+    assert False
